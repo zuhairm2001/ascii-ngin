535e6dde4d33de4650a51aeb8b5144ae8ef11cdf
zuhairm2001 <zuhairm2001@hotmail.com>
2026-01-18 13:29:56 +1100

    refactor to golang image lib for image transformation functionalities

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/zuhairm2001/ascii-ngin
 
 go 1.24.5
 
-require google.golang.org/genai v1.40.0
+require (
+	golang.org/x/image v0.34.0
+	google.golang.org/genai v1.40.0
+)
 
 require (
 	cloud.google.com/go v0.116.0 // indirect
@@ -15,7 +18,6 @@ require (
 	github.com/gorilla/websocket v1.5.3 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
-	golang.org/x/image v0.34.0 // indirect
 	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.32.0 // indirect
--- internal/video/setup.go
+++ internal/video/setup.go
@@ -10,7 +10,7 @@ type TermData struct {
 	Height int
 }
 
-// making sure all of our deps are installed, assuming UNIX like system
+// CheckDependencies verifies required system dependencies are installed
 func CheckDependencies() error {
 	err := checkFFmpegInstalled()
 	if err != nil {
@@ -18,32 +18,7 @@ func CheckDependencies() error {
 		return err
 	}
 
-	err = checkMagickInstalled()
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-
-	err = checkAwkInstalled()
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-
-	err = checkFindInstalled()
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-
-	err = checkTailInstalled()
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-
 	return nil
-
 }
 
 func GetTerminalDimensions() (TermData, error) {
@@ -69,38 +44,6 @@ func checkFFmpegInstalled() error {
 	_, err := exec.LookPath("ffmpeg")
 	if err != nil {
 		return fmt.Errorf("ffmpeg is not installed or not found in PATH")
-	}
-	return nil
-}
-
-func checkMagickInstalled() error {
-	_, err := exec.LookPath("magick")
-	if err != nil {
-		return fmt.Errorf("ImageMagick is not installed or not found in PATH")
-	}
-	return nil
-}
-
-func checkAwkInstalled() error {
-	_, err := exec.LookPath("awk")
-	if err != nil {
-		return fmt.Errorf("awk is not installed or not found in PATH")
-	}
-	return nil
-}
-
-func checkFindInstalled() error {
-	_, err := exec.LookPath("find")
-	if err != nil {
-		return fmt.Errorf("find is not installed or not found in PATH")
-	}
-	return nil
-}
-
-func checkTailInstalled() error {
-	_, err := exec.LookPath("tail")
-	if err != nil {
-		return fmt.Errorf("tail is not installed or not found in PATH")
 	}
 	return nil
 }
